9dcb41f59ac4e3c9844506df0e304d0788b091b5
Anthony Saltarelli <anthonysaltarelli@users.noreply.github.com>
2025-11-25 21:46:46 -0500

    editor updates

--- components/tiptap-templates/notion-like/notion-like-editor-header.tsx
+++ components/tiptap-templates/notion-like/notion-like-editor-header.tsx
@@ -1,37 +1,34 @@
 "use client"
 
-import { ThemeToggle } from "@/components/tiptap-templates/notion-like/notion-like-editor-theme-toggle"
-
 // --- Tiptap UI ---
 import { UndoRedoButton } from "@/components/tiptap-ui/undo-redo-button"
 
 // --- UI Primitives ---
 import { Spacer } from "@/components/tiptap-ui-primitive/spacer"
-import { Separator } from "@/components/tiptap-ui-primitive/separator"
 import { ButtonGroup } from "@/components/tiptap-ui-primitive/button"
 
 // --- Styles ---
 import "@/components/tiptap-templates/notion-like/notion-like-editor-header.scss"
 
-import { CollaborationUsers } from "@/components/tiptap-templates/notion-like/notion-like-editor-collaboration-users"
-
 export function NotionEditorHeader() {
   return (
     <header className="notion-like-editor-header">
+      <div className="notion-like-editor-header-title">
+        <span style={{ 
+          fontSize: '0.875rem', 
+          fontWeight: 500, 
+          color: '#6b7280',
+          paddingLeft: '1rem'
+        }}>
+          Content Editor
+        </span>
+      </div>
       <Spacer />
       <div className="notion-like-editor-header-actions">
         <ButtonGroup orientation="horizontal">
           <UndoRedoButton action="undo" />
           <UndoRedoButton action="redo" />
         </ButtonGroup>
-
-        <Separator />
-
-        <ThemeToggle />
-
-        <Separator />
-
-        <CollaborationUsers />
       </div>
     </header>
   )
